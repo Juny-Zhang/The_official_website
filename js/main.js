567f1b92c68afc37ccd240a7e18ffd11b7afa008
Juny-Zhang <1316261068@qq.com>
2016-12-28 15:57:56 +0800

    code

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// alert("我要学习前端");
+// alert("学习");
+// alert(886);
+// console.log("今天天气真好");
+
+var a = 100;
+var b = "study"
+console.log(typeof a);
+console.log(typeof b);
+
+console.log("我"+"爱"+"你");
+
+console.log(1+2+3);
+	a = b ;
+console.log(a);
+
